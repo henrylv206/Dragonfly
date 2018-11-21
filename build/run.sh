8b61f040e9e7b88b12557f039c628ca7b07710b5
lvhonglei <lvhonglei@jd.com>
2018-11-21 11:49:10 +0800

    update dragonfly client

--- build/run.sh
+++ build/run.sh
@@ -1,23 +1,38 @@
 #! /bin/bash
 
 # usage
-echo "docker run --privileged -d -v /etc/hosts:/etc/node/hosts -e SUPERNODE_IPS=$supernode_ips -e REGISTRY=$registry -p 80:80 henrylv206@dragonfly-client:latest"
+echo "docker run --privileged --net=host -d -v /etc/hosts:/etc/node/hosts -e SUPERNODE_IPS=$supernode_ips -e REGISTRY=$registry -e PORT=$port j-hub.jd.com/jdevops/dragonfly-client:latest"
+
+# delete last time's hosts
+sed '/##dragonfly##$/ d' /etc/node/hosts > /etc/node/hosts
 
 # dfget config
-echo [node] > /etc/dragonfly.conf
-echo address=$SUPERNODE_IPS >> /etc/dragonfly.conf
+if [[ -z "${SUPERNODE_IPS}" ]]; then
+  echo "${SUPERNODE_IPS} must be valid ip, eg: 1.2.3.4,5.6.7.8"
+  exit 1
+else
+  # TODO valid check
+  echo [node] > /etc/dragonfly.conf
+  echo address=$SUPERNODE_IPS >> /etc/dragonfly.conf
+fi
+
+# check port
+port=${PORT:-80}
+
+netstat -ltnp | grep -w ":${port}"
+if [ $? -eq 0 ]; then
+  echo "port ${port} is in use."
+  exit 1
+fi
 
 # update hosts
-# TODO default registry j-hub.jd.com
-# 1. 127.0.0.1
-echo 127.0.0.1 j-hub.jd.com >> /etc/node/hosts
-
-# TODO 2. set pod ip 
-
+if [[ -z "${REGISTRY}" ]]; then
+  echo "${REGISTRY} must be registry, eg: j-hub.jd.com"
+  exit 1
+else
+  echo 127.0.0.1 ${REGISTRY} ##dragonfly## >> /etc/node/hosts
+fi
 
 # dfdaemon config
-# TODO default port 80
-
-dfdaemon --port 80
-
+dfdaemon --port ${port}
 
